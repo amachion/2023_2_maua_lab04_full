67dfbcedb27397ee3690f5016b87cfd4740e805c
Andreia <andreia.machion@saojudas.br>
2023-10-23 12:52:21 -0300

    ap_10 até 2.7

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let filmes = [
+    {
+    titulo: "Forrest Gump - O Contador de Histórias",
+    sinopse: "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks),um rapaz com QI abaixo da média e boas intenções."
+    },
+    {
+    titulo: "Um Sonho de Liberdade",
+    sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
+    },
+]
+
+//ponto de acesso para req get teste
+app.get('/oi', (req, res) => res.send('oi'));
+
+//ponto de acesso para carregar a lista de filmes cadastrados
+app.get('/filmes', (req, res) => res.send(filmes))
+
+//ponto de acesso para incluir um novo filme à lista de filmes
+app.post('/filmes', (req, res) => {
+    const titulo = req.body.titulo;
+    const sinopse = req.body.sinopse;
+    const filme = {titulo: titulo, sinopse: sinopse};
+    filmes.push(filme);
+    //só para conferir
+    res.json(filmes)
+})
+
+app.listen (3000, () => console.log ("aplicação up and running"));
