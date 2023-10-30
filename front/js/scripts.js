3bae65a0cb85d0dd72403a017cce076ceff1d820
Andreia <andreia.machion@saojudas.br>
2023-10-30 12:44:17 -0300

    aula 30/10 até código 2.13.1

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -5,5 +5,15 @@ const endPoint = '/filmes';
 async function obterFilmes () {
     const urlCompleta = `${protocolo}${host}${endPoint}`;
     const filmes = (await axios.get(urlCompleta)).data;
-    console.log(filmes)
+    //console.log(filmes)
+    let tabela = document.querySelector('.filmes');
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0];
+    for (filme of filmes) {
+        let linha = corpoTabela.insertRow(0);
+        let celulaTitulo = linha.insertCell(0);
+        let celulaSinopse = linha.insertCell(1);
+        celulaTitulo.innerHTML=filme.titulo;
+        celulaSinopse.innerHTML=filme.sinopse;
+    }
+
 }
