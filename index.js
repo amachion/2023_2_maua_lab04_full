e4c4e83ce75f9b6b6f94fb18fc63e8b6eaae2dbd
Andreia Cristina Grisolio Machion <andreia.machion@maua.br>
2023-11-06 13:14:09 -0300

    aula 06/11 até 2.19.2

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-let filmes = [
-    {
-    titulo: "Forrest Gump - O Contador de Histórias",
-    sinopse: "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks),um rapaz com QI abaixo da média e boas intenções."
-    },
-    {
-    titulo: "Um Sonho de Liberdade",
-    sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
-    },
-]
+const Filme = mongoose.model ("Filme", mongoose.Schema({
+    titulo: {type: String},
+    sinopse: {type: String}
+}))
+
+async function conectarMongoDB () {
+    await mongoose.connect(`mongodb+srv://pro_mac:mongo123@cluster0.skf8n.mongodb.net/?retryWrites=true&w=majority`)
+}
 
 //ponto de acesso para req get teste
 app.get('/oi', (req, res) => res.send('oi'));
 
 //ponto de acesso para carregar a lista de filmes cadastrados
-app.get('/filmes', (req, res) => res.send(filmes))
+app.get('/filmes', async (req, res) => {
+    const filmes = await Filme.find();
+    res.send(filmes)
+})
 
 //ponto de acesso para incluir um novo filme à lista de filmes
-app.post('/filmes', (req, res) => {
+app.post('/filmes', async (req, res) => {
     const titulo = req.body.titulo;
     const sinopse = req.body.sinopse;
-    const filme = {titulo: titulo, sinopse: sinopse};
-    filmes.push(filme);
-    //só para conferir
+    const filme = new Filme ({titulo: titulo, sinopse: sinopse});
+    
+    await filme.save()
+    const filmes = await Filme.find()
     res.json(filmes)
 })
 
-app.listen (3000, () => console.log ("aplicação up and running"));
+app.listen (3000, () => {
+    try {
+        conectarMongoDB();
+        console.log ("aplicação up and running")
+    }
+    catch (e) {
+        console.log ("ups: ", e)
+    }
+});
